d11a6390ac6e20847d21ef40357c74619baa1cba
yqpan1991 <shifengzhongdexiao@163.com>
2015-09-17 22:44:50 +0800

    yqpan : init project

---
+++ image/src/main/java/com/edus/apollo/funny/MainActivity.java
@@ -0,0 +1,13 @@
+package com.edus.apollo.funny;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
